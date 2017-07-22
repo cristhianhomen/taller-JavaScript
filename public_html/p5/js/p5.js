d15f8bcb357be596894d55a180403c83ea7afee1
ADSI <ADSI@CARDFPCTI7F247.FormacionSena.RED>
2017-07-22 17:33:04 -0500

    trabajo realizado con exito

---
+++ public_html/p5/js/p5.js
@@ -0,0 +1,6 @@
+var nombre=prompt("ingrese su nombre completo");
+console.log("su nombre completo es"+nombre+"");
+var tipo=prompt("ingrese su tipo de documento");
+console.log("su tipo de documento es"+tipo+"");
+var numero=prompt("ingrese su numero de documento");
+console.log("su numero de documento es"+numero+"");
