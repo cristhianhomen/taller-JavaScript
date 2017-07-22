d058015592c1bedf164165de32f0af201a5ae6d2
ADSI <ADSI@CARDFPCTI7F247.FormacionSena.RED>
2017-07-22 17:32:57 -0500

    trabajo realizado con exito

---
+++ public_html/p4/js/p4.js
@@ -0,0 +1,19 @@
+var nombre=prompt("ingrese su nombre completo");
+console.log("hola,mi nombre es "+nombre+"");
+var tipo=prompt("ingrese tipo de documento");
+console.log("su tipo de documento es"+tipo+"");
+var numero=prompt("ingrese numero de documento");
+console.log("su numero de documento es"+numero+"");
+var genero=prompt("ingrese genero biologico");
+console.log("su genero biologico es"+genero+"");
+var estatura=prompt("ingrese su estatura");
+console.log("su estatura es"+estatura+"");
+var locacion=prompt("escriba f o v si usted vive en cartago?");
+if(locacion=="f" || locacion=="F");
+{
+console.log("usted no vive en cartago");
+}
+if(locacion=="v" || locacion=="V")
+{
+console.log("usted si vive en cartago");
+}
